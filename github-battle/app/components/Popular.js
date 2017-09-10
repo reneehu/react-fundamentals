770a250dc5d317c171d14b8d15165f1e58adc840
reneehu <reneehu217@gmail.com>
2017-09-10 10:11:03 -0400

    update

--- github-battle/app/components/Popular.js
+++ github-battle/app/components/Popular.js
@@ -2,55 +2,55 @@ var React = require('react');
 var PropsTypes = require('prop-types');
 
 function SelectLanguage(props) {
-	var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
-	return (
-			<ul className='languages'>
-			{
-			languages.map(function(lang){
-				return(
-					<li 
-					style={lang === props.selectedLanguage? {color:'#d0021b'} : null}
-					onClick={props.onSelect.bind(null,lang)}
-					key={lang}>
-						{lang}
-					</li>
-					)
-			})}
-			</ul>
-	)	
+  var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+  return (
+      <ul className='languages'>
+      {
+      languages.map(function(lang){
+        return(
+          <li 
+          style={lang === props.selectedLanguage? {color:'#d0021b'} : null}
+          onClick={props.onSelect.bind(null,lang)}
+          key={lang}>
+            {lang}
+          </li>
+          )
+      })}
+      </ul>
+  ) 
 }
 
 SelectLanguage.PropsTypes = {
-	selectedLanguage : PropsTypes.string.isRequired,
-	onSelect : PropsTypes.func.isRequired
+  selectedLanguage : PropsTypes.string.isRequired,
+  onSelect : PropsTypes.func.isRequired
 }
 
 class Popular extends React.Component {
-	constructor(props) {
-		super(props);
-	    this.state = {
-	      selectedLanguage: 'All',
-	    };
-	    this.updateLanguage = this.updateLanguage.bind(this);
-	}
-	updateLanguage(lang) {
-		this.setState(function() {
-			return {
-				selectedLanguage : lang,
-			}
-		});
-	}
+  constructor(props) {
+    super(props);
+      this.state = {
+        selectedLanguage: 'All',
+      };
+      this.updateLanguage = this.updateLanguage.bind(this);
+  }
+  updateLanguage(lang) {
+    this.setState(function() {
+      return {
+        selectedLanguage : lang,
+      }
+    });
+  }
 
-	render() {
-		return (
-			<div>
-			 	<SelectLanguage 
-			 	selectedLanguage = {this.state.selectedLanguage}
-			 	onSelect = {this.updateLanguage}
-			 	/>
-			</div>
-			)
-	}
+  render() {
+    return (
+      <div>
+        <SelectLanguage 
+        selectedLanguage = {this.state.selectedLanguage}
+        onSelect = {this.updateLanguage}
+        />
+      </div>
+      )
+  }
 }
 
 module.exports = Popular;
